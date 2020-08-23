14db50f0c54bfa13408269ca220bae29bbf8b910
Bikram2055 <65553888+Bikram2055@users.noreply.github.com>
2020-08-23 09:07:50 +0545

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'z3a+(su&un&u)$rf*hovexwoq8@5ujxig%!3u!)2a)*f6rr0%4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
